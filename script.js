31a5f274e6a1cf8fc14f500ed91ba95c8d7e8656
roamingterra <danielballerini1@gmail.com>
2022-11-21 20:48:38 -0500

    Complete extra credit
    The functionality of the etch-a-sketch game is now 100% complete. This
    extra credit involved darkening the square, after it was assigned a
    random colour, by 10% after each following pass of the cursor. This
    involved the initialization of a new array and temporary variables to
    store in memory the original colour value, and the use of regex
    expressions to receive and modify colour values via temporary variables.
    Finally, a conditional statement was used in the event listener to
    determine whether it was the first time passing over a square or not.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function generateEtchASketch(size){
     //Populate the arrays based on the chosen resolution
     for(let i=0; i<size; i++){ //JavaScript doesn't have multi-dimentional arrays, but you can store objects/arrays in an array
         squares[i]=[];
+        colors[i]=[];
     }
     //Create 16 div elements using a for loop. Give each element an ID of a number from 1-16
     for(let i=0; i<size; i++){
@@ -13,19 +14,38 @@ function generateEtchASketch(size){
         for(let j=0; j<size; j++){
             squares[i][j] = document.createElement('span');
             squares[i][j].classList.add(`square`);
-            squares[i][j].classList.add(`${i+1}X${j+1}`);
+            squares[i][j].classList.add(`${i}X${j}`); //`${i+1}X${j+1}`
             //squares[i][j].textContent = "*"; //Placeholder text for now to display something in the page
             rows[i].appendChild(squares[i][j]);
         }
     }
 }
 
+function randomRGB (){
+    let num = Math.floor(Math.random()*256); //RBG has values between 0 and 256
+    return num;
+}
+
+function divideByTen (num){ //Used for the gradual darkening of each square
+    return (Math.ceil(num/10));
+}
+
 //Statically created element in the html file
 let etchASketch = document.querySelector('#etch-a-sketch');
 
-//Create an array to contain all of the 16 div elements, and an array to contain all of the span elements
+//Create an array to contain all of the div elements, and an array to contain all of the span elements
 let rows = [];
 let squares = [];
+
+//Create array to keep track of random RGB colour assigned to square, and two variables to keep track of position in array
+colors = [];
+let i = 0;
+let j = 0;
+
+//Initialize variables for red, green, and blue to find the number to reduce from the original rgb colours
+let red = 0;
+let green = 0;
+let blue = 0;
 
 //Call on function to generate the Etch-A-Sketch game
 generateEtchASketch(16);
@@ -54,9 +74,36 @@ btn.addEventListener('click', () => {
 //propagation. Add the event listener to a statically created parent, and commit the event to the dynamic
 //children through the use of conditional statements.
 
+
 etchASketch.addEventListener('mouseover', (event) => {
     if(event.target.matches('.square')){
-        event.target.style.backgroundColor = 'black';
-        console.log(event.target);
+        if(event.target.style.backgroundColor===""){ //Square will only be given a new colour if it's white
+            event.target.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`; //Assign square random colour
+
+            i = event.target.className.match('[0-9]');
+            j = event.target.className.match('(?<=X)[0-9]');
+
+            colors[i][j] = event.target.style.backgroundColor; //Save randomly assigned square colour in memory
+
+            console.log(colors[i][j]);
+        }
+        else{
+            //Get the RGB colour from the array, divide each number by ten, then take that number and subtract it from the element's current number
+            i = event.target.className.match('[0-9]'); //Match first dimension of array from class name and store in temp variable
+            j = event.target.className.match('(?<=X)[0-9]'); //Match second dimension of array from class name and store in temp variable
+
+            red = event.target.style.backgroundColor.match('[0-9]+?(?=,)') //Match rgb red number from square element, and store in temp variable
+            green = event.target.style.backgroundColor.match('(?<=, )[0-9]*?(?=,)');//Match rgb green number from square element, and store in temp variable
+            blue = event.target.style.backgroundColor.match('(?<=, )[0-9]*?(?=[)])');//Match rgb blue number from square element, and store in temp variable
+
+            red = red - (divideByTen(colors[i][j].match('[0-9]+?(?=,)'))); //New red becomes old red minus dividing number obtained from original red
+            green = green - (divideByTen(colors[i][j].match('(?<=, )[0-9]*?(?=,)'))); //New green becomes old green minus dividing number obtained from original green
+            blue = blue - (divideByTen(colors[i][j].match('(?<=, )[0-9]*?(?=[)])'))); //New blue becomes old blue minus dividing number obtained from original blue
+
+            event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; //update square colour
+
+            console.log(event.target.style.backgroundColor);
+        }
     }
 })
+
